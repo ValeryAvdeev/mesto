d173610b5182498d308eaa10b5a555dba7a951a7
Valery Avdeev <vkavdeev@gmail.com>
2021-11-14 20:44:13 +0300

    готовый проект

---
+++ script/index.js
@@ -0,0 +1,39 @@
+let btnPoppu = document.querySelector(".button_popup");
+let popup = document.querySelector(".popup");
+let popupCloseBtn = popup.querySelector(".popup__close");
+
+function openPopup() {
+  popup.classList.add("popup_open");
+}
+
+btnPoppu.addEventListener("click", openPopup);
+
+function closePopup() {
+  popup.classList.remove("popup_open");
+}
+
+popupCloseBtn.addEventListener("click", closePopup);
+
+// Находим форму в DOM
+const formElement = document.querySelector(".form_js"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".form__input_popup_name"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".form__input_popup_job"); // Воспользуйтесь инструментом .querySelector()
+let nameProfile = document.querySelector(".profile__title_name");
+let jodProfile = document.querySelector(".profile__subtitle_job");
+
+nameInput.value = nameProfile.textContent;
+jobInput.value = jodProfile.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+  // Вставьте новые значения с помощью textContent
+  nameProfile.textContent = nameInput.value;
+  jodProfile.textContent = jobInput.value;
+
+  closePopup();
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
