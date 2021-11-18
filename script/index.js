7d36edd85ee47d274cf00545bcc795de2146699b
Valery Avdeev <vkavdeev@gmail.com>
2021-11-18 18:03:03 +0300

    Исправление ошибок после ревью

--- script/index.js
+++ script/index.js
@@ -1,9 +1,18 @@
-let btnPoppu = document.querySelector(".button_popup");
+let btnPoppu = document.querySelector(".button_item_edit");
 let popup = document.querySelector(".popup");
 let popupCloseBtn = popup.querySelector(".popup__close");
 
+const formElement = document.querySelector(".form");
+let nameInput = formElement.querySelector(".form__input_popup_name");
+let jobInput = formElement.querySelector(".form__input_popup_job");
+
+let nameProfile = document.querySelector(".profile__title_popup_name");
+let jodProfile = document.querySelector(".profile__subtitle_popup_job");
+
 function openPopup() {
   popup.classList.add("popup_open");
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jodProfile.textContent;
 }
 
 btnPoppu.addEventListener("click", openPopup);
@@ -13,17 +22,6 @@ function closePopup() {
 }
 
 popupCloseBtn.addEventListener("click", closePopup);
-
-// Находим форму в DOM
-const formElement = document.querySelector(".form_js"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".form__input_popup_name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".form__input_popup_job"); // Воспользуйтесь инструментом .querySelector()
-let nameProfile = document.querySelector(".profile__title_name");
-let jodProfile = document.querySelector(".profile__subtitle_job");
-
-nameInput.value = nameProfile.textContent;
-jobInput.value = jodProfile.textContent;
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
